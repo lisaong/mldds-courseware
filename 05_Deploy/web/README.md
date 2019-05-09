95238d3851426dddeb95f4eb3c4129a7438abb8c
Lisa Ong <issohl@nus.edu.sg>
2019-05-09 14:04:05 +0800

    updated deployment example

--- 05_Deploy/web/README.md
+++ 05_Deploy/web/README.md
@@ -1,12 +1,33 @@
+## Web Deployment of Machine Learning Model
+
+Here is a simple example illustrating how a trained Machine Learning model can be deployed in the backend of a web server.
+
+![architecture](architecture.png)
+
+Using the browser or a mobile app, a user sends raw input data to the web application (running on a Flask server, for example). The web application queries the trained model, gets predictions, and returns the results back to the user.
+
 ## Setup
 ```
 pip install Flask
+```
+
+## Generate model
+```
+pushd ..
+python sklearn_example.train.py
+popd
 ```
 
 ## Run
 ```
 python myapp.py
 ```
+
+Go to http://127.0.0.1:5000 from your browser.
+
+Code Layout:
+* myapp.py: a simple Flask application. This supports a POST and GET request to the /form
+* models.py: a python class that loads a pre-trained model (trained using sklearn_example.train.py in the parent folder), and uses the model to perform predictions.
 
 ## References
 - http://flask.pocoo.org/docs/1.0/quickstart/
--- 05_Deploy/web/myapp.py
+++ 05_Deploy/web/myapp.py
@@ -6,17 +6,7 @@ from models import AutoMpg_Sklearn
 app = Flask(__name__)
 model = AutoMpg_Sklearn()
 
-@app.route('/')
-def hello():
-    df = pd.read_csv('../auto-mpg.data-nans.txt',
-                    delim_whitespace=True, # the data uses whitespaces as separators instead of commas
-                    na_values=['?', 'NA'],  # to handle '?' in the horsepower column
-                    names=['mpg', 'cylinders', 'displacement', 'horsepower', # data has no column names
-                            'weight', 'acceleration', 'model_year', 'origin', 'car_name'])
-
-    return repr(model.predict(df.loc[:, 'cylinders':'origin']))
-
-@app.route('/form', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def form_example():
     if request.method == 'POST':
         X = [int(request.form.get('cylinders')),
@@ -28,15 +18,14 @@ def form_example():
             int(request.form.get('origin'))]
 
         prediction = model.predict([X]).ravel()[0]
+        return render_template('prediction.html', parameters=dict(request.form),
+            prediction=prediction)
 
-        return f"""Prediction for {dict(request.form)}:<br>
-             {prediction:.3f} mpg"""
-
-    # render a jinja template using pre-defined parameters
-    return render_template('form.html',
-        model_year_min=70, model_year_max=82,
-        cylinder_options=[4, 6, 8],
-        origin_options=[1, 2, 3])
+    else:
+        return render_template('form.html',
+            model_year_min=70, model_year_max=82,
+            cylinder_options=[4, 6, 8],
+            origin_options=[1, 2, 3])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) # run app in debug mode on port 5000
